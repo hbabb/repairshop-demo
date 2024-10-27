28193ab48cbc5f32c1345c9a88febdcdc46dc06d
Heath Babb <hbabb@hbc-data.com>
2024-10-26 22:18:40 -0400

    feat: Project configuration and basic layouts completed; Setup Sentry for Error tracing | Lesson One Complete

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bb6962d92f8ec1795824e660635751f9@o4508192074432512.ingest.us.sentry.io/4508192086032384",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
